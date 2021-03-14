44bc41ed59095364329ee6ff768b3e2db8dd9952
kylpo <2kylepoole@gmail.com>
2021-03-13 23:03:07 -0700

    POC that jumps between file and its test. Creates test folder and file if needed.

--- src/extension.ts
+++ src/extension.ts
@@ -21,99 +21,69 @@ export function activate(context: vscode.ExtensionContext) {
         return;
       }
 
-      // See https://github.com/bgse/relative-path-and-line/blob/master/src/extension.ts#L34
-      // get workspace relative path
-      // let path = vscode.workspace.asRelativePath(doc.fileName);
-      // let path = doc.fileName.replace(/\.[^/.]+$/, "")
-      // vscode.commands.executeCommand(
-      // 	'workbench.action.quickOpen',
-      // 	path
-      // );
+      const { dir, ext, name } = path.parse(doc.fileName);
 
-      // const filePath = path.parse(doc.fileName);
-      const { dir, ext, name } = path.parse(
-        vscode.workspace.asRelativePath(doc.fileName)
-      );
+      const config = {
+        Component: {
+          // parentDir: "/",
+          // ext: [".ts", ".tsx"],
+        },
+        Test: {
+          parentDir: "__tests__",
+          nameExt: "test",
+        },
+        Stories: {
+          parentDir: "__stories__",
+          nameExt: "stories",
+        },
+      };
 
-      const blockList = vscode.workspace.getConfiguration(
-        "extension.openRelated"
-      ).subdirectories;
+      // See https://github.com/AlanWalk/quick-open-file/blob/master/src/utils.ts
+      vscode.window
+        .showQuickPick(Object.keys(config), { matchOnDetail: true })
+        .then((option) => {
+          const dirPath = dir.split(path.sep);
+          const parentDir = dirPath.pop();
 
-      // const blockList = ["__tests__", "__stories__"];
-      const dirPath = dir.split(path.sep);
-      const parentDir = dirPath.pop();
+          if (parentDir == "__tests__") {
+            vscode.workspace
+              .openTextDocument(
+                `${dirPath.join(path.sep)}/${name.split(".")[0]}${ext}`
+              )
+              .then((editor) => {
+                if (!editor) return;
+                vscode.window.showTextDocument(editor);
+              });
+            return;
+          }
+          const testPath = `${dir}/__tests__/${name}.test${ext}`;
 
-      const query =
-        parentDir != null && blockList.includes(parentDir)
-          ? `${dirPath.join(path.sep)}/${name.split(".")[0]}`
-          : `${dir}/${name.split(".")[0]}`;
+          if (fs.existsSync(testPath)) {
+            vscode.workspace.openTextDocument(testPath).then((editor) => {
+              if (!editor) return;
+              vscode.window.showTextDocument(editor);
+            });
+          } else {
+            try {
+              const folderPath = `${dir}/__tests__/`;
+              // create the directory
+              if (!fs.existsSync(folderPath)) {
+                fs.mkdirSync(folderPath);
+              }
+              fs.writeFileSync(testPath, "" /*content*/);
 
-      vscode.commands.executeCommand("workbench.action.quickOpen", query);
+              vscode.workspace.openTextDocument(testPath).then((editor) => {
+                if (!editor) return;
+                vscode.window.showTextDocument(editor);
+              });
+            } catch (err) {
+              // log?
+              console.log("Error", err.message);
 
-      // let filename = doc.fileName.replace(/\.[^/.]+$/, "")
-      // const { dir, ext, name } = path.parse(doc.fileName);
-
-      // const config = {
-      //   Component: {
-      //     // parentDir: "/",
-      //     // ext: [".ts", ".tsx"],
-      //   },
-      //   Test: {
-      //     parentDir: "__tests__",
-      //     nameExt: "test",
-      //   },
-      //   Stories: {
-      //     parentDir: "__stories__",
-      //     nameExt: "stories",
-      //   },
-      // };
-
-      // // See https://github.com/AlanWalk/quick-open-file/blob/master/src/utils.ts
-      // vscode.window
-      //   .showQuickPick(Object.keys(config), { matchOnDetail: true })
-      //   .then((option) => {
-      //     const dirPath = dir.split(path.sep);
-      //     const parentDir = dirPath.pop();
-
-      //     if (parentDir == "__tests__") {
-      //       vscode.workspace
-      //         .openTextDocument(
-      //           `${dirPath.join(path.sep)}/${name.split(".")[0]}${ext}`
-      //         )
-      //         .then((editor) => {
-      //           if (!editor) return;
-      //           vscode.window.showTextDocument(editor);
-      //         });
-      //       return;
-      //     }
-      //     const testPath = `${dir}/__tests__/${name}.test${ext}`;
-
-      //     if (fs.existsSync(testPath)) {
-      //       vscode.workspace.openTextDocument(testPath).then((editor) => {
-      //         if (!editor) return;
-      //         vscode.window.showTextDocument(editor);
-      //       });
-      //     } else {
-      //       try {
-      //         const folderPath = `${dir}/__tests__/`;
-      //         // create the directory
-      //         if (!fs.existsSync(folderPath)) {
-      //           fs.mkdirSync(folderPath);
-      //         }
-      //         fs.writeFileSync(testPath, "" /*content*/);
-
-      //         vscode.workspace.openTextDocument(testPath).then((editor) => {
-      //           if (!editor) return;
-      //           vscode.window.showTextDocument(editor);
-      //         });
-      //       } catch (err) {
-      //         // log?
-      //         console.log("Error", err.message);
-
-      //         throw err;
-      //       }
-      //     }
-      //   });
+              throw err;
+            }
+          }
+        });
     }
   );
 
